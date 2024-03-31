a50ca947ee2e49f98c0b4a73964551635921781e
Antonio Maradiaga <antonio@maradia.ga>
2024-03-31 16:52:59 +0200

    Generating event and posting it to HTTP endpoint

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const app = require('./app');
 
+require('dotenv').config()
+
 const nodeAppStarted = Date.now();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Mock server started on port ${port} after ${Date.now() - nodeAppStarted} ms, running - stop with CTRL+C (or CMD+C)...`))
+
+console.log(`Simulating Business Partner Created events -> ${process.env.SIMULATE_BP_CREATED}`);
+
+if (process.env.SIMULATE_BP_CREATED === 'true') {
+    const simulateBPCreated = require('./simulateBPCreated');
+    const data = require('./business-partner/business-partner-data.js').data;
+
+    const url = process.env.SIMULATE_EVENTS_URL;  // replace your endpoint
+    const auth = { username: process.env.SIMULATE_EVENTS_USERNAME, password: process.env.SIMULATE_EVENTS_PASSWORD };  // replace with your credentials
+
+    const cronSchedule = process.env.SIMULATE_BP_CREATED_CRON || '*/30 * * * * *';  // default is every 30 seconds
+
+    console.log(`Events will be generated on the following schedule: ${cronSchedule}\n`);
+
+    simulateBPCreated(cronSchedule, url, auth, data);
+}
---
+++ simulateBPCreated.js
@@ -0,0 +1,58 @@
+const https = require('https');
+const cron = require('node-cron');
+const { CloudEvent } = require('cloudevents');
+
+// Assuming the data items are in an array
+// const items = Array.from({length: 1000}, (_, i) => `item_${i}`);
+let currentItemIndex = 0;
+
+const post_data = (url, auth, item) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + Buffer.from(auth.username + ':' + auth.password).toString('base64')
+        }
+    };
+
+    bp = item['item']
+    bpId = bp['BusinessPartner']
+
+    const event = new CloudEvent({
+        type: 'sap.s4.beh.businesspartner.v1.BusinessPartner.Created.v1',
+        specversion: '1.0',
+        source: '/default/sap.s4.beh/244572008',
+        datacontenttype: 'application/json',
+        data: {
+            BusinessPartner: bpId
+        }
+    });
+
+    console.log(`Posting event for Business Partner ${bpId}`);
+
+    const req = https.request(url, options, (res) => {
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
+
+    req.on('error', (error) => {
+        console.error(error);
+    });
+
+    req.write(event.toString());
+    req.end();
+};
+
+const startPosting = (cronSchedule, url, auth, data) => {
+    cron.schedule(cronSchedule, () => {
+        console.log('Running cron job');
+        console.log(`Current item index: ${currentItemIndex} and data.length: ${data.length}`);
+        if (currentItemIndex < data.length) {
+            const item = data[currentItemIndex++];
+            post_data(url, auth, { item });
+        }
+    });
+};
+
+module.exports = startPosting;
